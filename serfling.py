a06aca7c68ddee7291b81d6a53d10cc438f5df39
patricion <patricionoyola@gmail.com>
2020-12-30 14:48:01 -0600

    updated variable names

--- serfling.py
+++ serfling.py
@@ -48,15 +48,15 @@ def GetData(file_name, sheet=None):
         ValueError: If the provided sheet doesn't exist in the provided file
             or if a input data point is not conformed by numbers.
     """
-    wb = load_workbook(file_name)
-    sheets = wb.get_sheet_names()
+    excel_workbook = load_workbook(file_name)
+    sheets = excel_workbook.get_sheet_names()
     if sheet:
         if sheet not in sheets:
             print "ERROR: Sheet", sheet, "does not exist in file", file_name
             raise ValueError
     else:
         sheet = sheets[0]
-    ws = wb[sheet]
+    excel_worksheet = excel_workbook[sheet]
 
     data = []
 
@@ -65,91 +65,92 @@ def GetData(file_name, sheet=None):
     b_cell = "B" + str(i)
     c_cell = "C" + str(i)
 
-    while ws[a_cell].value is not None and ws[b_cell].value is not None:
-        if not isinstance(ws[a_cell].value, numbers.Number):
+    while excel_worksheet[a_cell].value is not None and excel_worksheet[b_cell].value is not None:
+        if not isinstance(excel_worksheet[a_cell].value, numbers.Number):
             print "ERROR: Value at cell", a_cell, "is not a number"
             raise ValueError
-        if not isinstance(ws[b_cell].value, numbers.Number):
+        if not isinstance(excel_worksheet[b_cell].value, numbers.Number):
             print "ERROR: Value at cell", b_cell, "is not a number"
             raise ValueError
 
-        if ws[c_cell].value == "Baseline":
-            data.append((ws[a_cell].value, ws[b_cell].value))
+        if excel_worksheet[c_cell].value == "Baseline":
+            data.append((excel_worksheet[a_cell].value, excel_worksheet[b_cell].value))
         i += 1
         a_cell = "A" + str(i)
         b_cell = "B" + str(i)
         c_cell = "C" + str(i)
 
     data.sort(key=(lambda x: x[0]))
-    t = [j[0] for j in data]
-    vals = [j[1] for j in data]
+    time_array = [j[0] for j in data]
+    value_array = [j[1] for j in data]
 
-    return np.array(t), np.array(vals)
+    return np.array(time_array), np.array(value_array)
 
 class Serlfling(object):
-    """Serfling sinusoidal regresion object."""
+    """Serfling sinusoidal regression object."""
 
-    def __init__(self, t, data, cycles, a=None, b1=None, b2=None, b3=None):
-        self.t = t
-        self.data = data
+    def __init__(self, times, values, cycles):
+        self.times = times
+        self.values = values
 
-        rang = t[-1] - t[0] + 1
-        period = rang / float(cycles)
+        range = times[-1] - times[0] + 1
+        period = range / float(cycles)
 
+        # Regression params: Y(t) = a + b1*t + b2*cos(2*t*pi / 52.1667) + f*sin(2*t*pi / 52.1667) + e
         self.p = period
-        self.a = a
-        self.b1 = b1
-        self.b2 = b2
-        self.b3 = b3
+        self.a = None
+        self.b1 = None
+        self.b2 = None
+        self.b3 = None
         self.e = 0
 
     def InitializeParameters(self):
-        """Initializes the serlfling sinusoidal regresion parameters.
+        """Initializes the serlfling sinusoidal regression parameters.
 
-        Sets the a, b1, b2, and b3 values of the regresion to initial approximations
+        Sets the a, b1, b2, and b3 values of the regression to initial approximations
         suited for optimization.
         """
-        lin_reg = linregress(self.t, self.data)
+        lin_reg = linregress(self.times, self.values)
         self.a = lin_reg[1]
         self.b1 = lin_reg[0]
-        self.b2 = 3 * np.std(data) / (2 ** 0.5)
+        self.b2 = 3 * np.std(self.values) / (2 ** 0.5)
         self.b3 = 0
         self.e = lin_reg[4]
 
     def OptimizeParameters(self):
         """Get the fitted alpha, beta 1, and beta 2 parameters.
 
-        Optimizes the current regresion parameters according to the input
+        Optimizes the current regression parameters according to the input
         time - value pairs.
 
         Parameters:
-            t: (numpy array) the data time values
-            data: (numpy array) data values corresponding to each of the time
+            times: (numpy array) the data time values
+            values: (numpy array) data values corresponding to each of the time
                 entries
         """
         optimize_func = lambda x: (self.a
-                                   + self.b1 * self.t
-                                   + x[0] * np.sin(self.t * (2 * np.pi / self.p))
-                                   + x[1] * np.cos(self.t * (2 * np.pi / self.p))
+                                   + self.b1 * self.times
+                                   + x[0] * np.sin(self.times * (2 * np.pi / self.p))
+                                   + x[1] * np.cos(self.times * (2 * np.pi / self.p))
                                    + self.e
-                                   - self.data)
+                                   - self.values)
         self.b2, self.b3, = leastsq(optimize_func, [self.b2, self.b3])[0]
 
     def rmse(self):
         e = (self.a
-             + self.b1 * self.t
-             + self.b2 * np.sin(self.t * (2 * np.pi / self.p))
-             + self.b3 * np.cos(self.t * (2 * np.pi / self.p))
+             + self.b1 * self.times
+             + self.b2 * np.sin(self.times * (2 * np.pi / self.p))
+             + self.b3 * np.cos(self.times * (2 * np.pi / self.p))
              + self.e
-             - self.data)
+             - self.values)
         return np.sqrt(np.mean((e) ** 2))
 
     def StdErrorDev(self):
         """Std dev of the error between the data and the regression."""
-        e = self.data - (self.a
-             + self.b1 * self.t
-             + self.b2 * np.sin(self.t * (2 * np.pi / self.p))
-             + self.b3 * np.cos(self.t * (2 * np.pi / self.p))
+        e = self.values - (self.a
+             + self.b1 * self.times
+             + self.b2 * np.sin(self.times * (2 * np.pi / self.p))
+             + self.b3 * np.cos(self.times * (2 * np.pi / self.p))
              + self.e
              )
 
@@ -164,10 +165,10 @@ class Serlfling(object):
         Params:
             x: desired percentile of values below the resulting curve.
         """
-        e = self.data - (self.a
-             + self.b1 * self.t
-             + self.b2 * np.sin(self.t * (2 * np.pi / self.p))
-             + self.b3 * np.cos(self.t * (2 * np.pi / self.p))
+        e = self.values - (self.a
+             + self.b1 * self.times
+             + self.b2 * np.sin(self.times * (2 * np.pi / self.p))
+             + self.b3 * np.cos(self.times * (2 * np.pi / self.p))
              + self.e
              )
 
@@ -176,11 +177,11 @@ class Serlfling(object):
         return e[int(n * x / 100)]
 
     def ValuesAbove(self, x = 0):
-        n = len(self.data)
+        n = len(self.values)
         j = 0
         f = self.GetSerflingRegresion()
         for i in xrange(n):
-            if f(self.t[i]) + x <= self.data[i]:
+            if f(self.times[i]) + x <= self.values[i]:
                 j += 1
         return j
 
@@ -196,7 +197,7 @@ class Serlfling(object):
 
     def GetPlotValues(self):
         f = self.GetSerflingRegresion()
-        data = [f(i) for i in self.t]
+        data = [f(i) for i in self.times]
         return np.array(data)
 
     def Print(self):
@@ -222,50 +223,50 @@ if len(sys.argv[:]) < 3:
     raise ValueError
 
 file_name = sys.argv[1]
-c = sys.argv[2]
+num_cycles = sys.argv[2]
 
 # We first estimate the regression parameters
-print "\nEstimating serfling regression parameters for data in file", file_name, "and", c, "cycles\n"
+print "\nEstimating serfling regression parameters for data in file", file_name, "and", num_cycles, "cycles\n"
 
-t, data = GetData(file_name)
-s = Serlfling(t, data, c)
-n = len(data)
+time_array, data_array = GetData(file_name)
+serfling = Serlfling(time_array, data_array, num_cycles)
+n = len(data_array)
 
-s.InitializeParameters()
-s.OptimizeParameters()
-s.Print()
+serfling.InitializeParameters()
+serfling.OptimizeParameters()
+serfling.Print()
 
 # Now we estimate useful curve offsets
 print ("Estimating percentile offsets and standard deviation of "
        "values above the curve.\n")
 
-percentile95_off = s.PercentileOffset(95)
+percentile95_off = serfling.PercentileOffset(95)
 print "- To get 95% of the values below the curve:\n"
 
 print "95% offset:", percentile95_off
-print "Values above the curve:", s.ValuesAbove(percentile95_off), "of", n
-print float(s.ValuesAbove(percentile95_off)) / n, "%\n"
+print "Values above the curve:", serfling.ValuesAbove(percentile95_off), "of", n
+print float(serfling.ValuesAbove(percentile95_off)) / n, "%\n"
 
-percentile975_off = s.PercentileOffset(97.5)
+percentile975_off = serfling.PercentileOffset(97.5)
 print "- To get 97.5% of the values below the curve:\n"
 
 print "97.5% offset:", percentile975_off
-print "Values above the curve:", s.ValuesAbove(percentile975_off), "of", n
-print float(s.ValuesAbove(percentile975_off)) / n, "%\n"
+print "Values above the curve:", serfling.ValuesAbove(percentile975_off), "of", n
+print float(serfling.ValuesAbove(percentile975_off)) / n, "%\n"
 
-std = s.StdErrorDev()
+std = serfling.StdErrorDev()
 p90 = 1.645
 
 print "- Standard deviation of the difference between the curve and the data\n"
 print "Std dev:", std, "\n"
 print "1.645 std deviation offset:", std * p90
-print "Values above the curve:", s.ValuesAbove(std * p90), "of", n
-print float(s.ValuesAbove(std * p90)) / n, "%\n"
+print "Values above the curve:", serfling.ValuesAbove(std * p90), "of", n
+print float(serfling.ValuesAbove(std * p90)) / n, "%\n"
 
 
 p975 = 2
 
 
 print "2 std deviation offset:", std * p975
-print "Values above the curve:", s.ValuesAbove(std * p975), "of", n
-print float(s.ValuesAbove(std * p975)) / n, "%\n"
+print "Values above the curve:", serfling.ValuesAbove(std * p975), "of", n
+print float(serfling.ValuesAbove(std * p975)) / n, "%\n"
